b2280ca6ae165e3be99022e1dbb7cef7a1f50b78
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-05-09 17:10:31 -0700

    adding basic etch-a-sketch functionality with mouseover changing grid unit color

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ const gridContainer = document.getElementById('grid-container')
 for (let i=0; i<16*16; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add('grid-unit');
-    gridContainer.appendChild(newDiv) 
+    newDiv.setAttribute('id', `${i}`);
+    gridContainer.appendChild(newDiv);
 }
+
+
+
+
+const gridUnit = document.querySelectorAll('.grid-unit');
+
+function activateColor(e) {
+    let activatedGridUnit = document.getElementById(e.target.id);
+    activatedGridUnit.classList.add('hover');
+}
+
+gridUnit.forEach((item) => {
+    item.addEventListener('mouseover', activateColor)
+})
+// gridUnit[i].addEventListener('mouseover', gridUnit[i].classList.add('hover'));
+
+
+
+
