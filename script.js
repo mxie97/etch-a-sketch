c68934daf5520b7b9a387c1f9445043572e34dd1
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-05-16 16:44:26 -0700

    make change grid density button that clears the current grid and repopulates according to user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.getElementById('grid-container')
+const changeGridButton = document.getElementById('change-grid')
 
 for (let i=0; i<16*16; i++) {
     const newDiv = document.createElement("div");
@@ -7,21 +8,49 @@ for (let i=0; i<16*16; i++) {
     gridContainer.appendChild(newDiv);
 }
 
+let gridUnits = document.querySelectorAll('.grid-unit');
 
+changeGridButton.addEventListener('click', changeGridDensity)
 
+function promptSquaresPerSide() {
+    let squaresPerSide = prompt("How many squares per side? Enter an integer between 16 and 100.");
+    return squaresPerSide;
+}
 
-const gridUnit = document.querySelectorAll('.grid-unit');
+function clearGrid() {
+    gridUnits.forEach((item) => {
+        item.remove()
+    });
+}
+
+function populateGrid(squaresPerSide) {
+    for (let i=0; i<squaresPerSide*squaresPerSide; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add('grid-unit');
+        newDiv.setAttribute('id', `${i}`);
+        gridContainer.appendChild(newDiv);
+    }    
+}
+
+function changeGridDensity() {
+    let numSquares = promptSquaresPerSide();
+    clearGrid();
+    populateGrid(numSquares);
+    gridUnits = document.querySelectorAll('.grid-unit');
+    let flexPercent = (1/numSquares)*100;
+    console.log(flexPercent);
+    gridUnits.forEach((item) => {
+        item.style.flexBasis = `${flexPercent}%`;
+    });
+
+}
 
 function activateColor(e) {
     let activatedGridUnit = document.getElementById(e.target.id);
     activatedGridUnit.classList.add('hover');
 }
 
-gridUnit.forEach((item) => {
+gridUnits.forEach((item) => {
     item.addEventListener('mouseover', activateColor)
 })
-// gridUnit[i].addEventListener('mouseover', gridUnit[i].classList.add('hover'));
-
-
-
 
